0ab10a333f1935d0cc66a292179db765e79a5ea4
mightyYaroslav <yaroslavsmth@gmail.com>
2018-01-18 18:20:11 +0200

    updated for linter

--- server/src/controllers/memeFromDb.ts
+++ server/src/controllers/memeFromDb.ts
@@ -3,14 +3,16 @@ import MemeModel from '../models/Meme'
 import { successRes, failRes } from '../utils/responses'
 
 export function memeFromDb (req: Request, res: Response, next: NextFunction) {
-  MemeModel.count().exec((err, count) => {
-    if (err)  
+  MemeModel.count({}).exec((err, count) => {
+    if (err) {
       res.json({ success: false, message: err })
+    }
     const random = Math.floor(Math.random() * count)
     MemeModel.findOne().skip(random).exec(
         (err, result) => {
-          if (err) 
+          if (err) {
             res.json({ success: false, message: err })
+          }
           res.json(successRes({ result }))
         })
   }
--- server/src/controllers/memeIncRate.ts
+++ server/src/controllers/memeIncRate.ts
@@ -4,13 +4,15 @@ import { successRes, failRes } from '../utils/responses'
 
 export function memeIncRating (req: Request, res: Response, next: NextFunction) {
   res.json(successRes(MemeModel.findById(req.query.id, (err, meme) => {
-    if (err) 
-    res.json({ success: false, message: err })
-      meme.rating++
-      meme.save((err, updated) => {
-            if (err) 
-            res.json({ success: false, message: err })
-        res.json(successRes({ updated }))
-      })
+    if (err) {
+      res.json({ success: false, message: err })
+    }
+    meme.rating++
+    meme.save((err, updated) => {
+      if (err) {
+        res.json({ success: false, message: err })
+      }
+      res.json(successRes({ updated }))
+    })
   })))
 }
