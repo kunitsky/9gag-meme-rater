9948994b098b98f95b9321e366e093d94c0492ae
Polo Abuser <poloabuser@Polos-MacBook-Air.local>
2018-01-19 01:42:50 +0200

    random arrays fix

--- server/build/controllers/randomMeme.js
+++ server/build/controllers/randomMeme.js
@@ -1,35 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = require("request-promise");
 const responses_1 = require("../utils/responses");
 const random_1 = require("../utils/random");
 const memesEndpoint = 'https://9gag.com/v1/group-posts/group/default/type/hot';
-exports.getRandomMemes = (req, res) => {
-    request(memesEndpoint).then(json => {
-        const responseMemes = JSON.parse(json).data.posts.filter(post => post.type !== 'Article');
-        const indecies = random_1.getRandomNumbers(responseMemes.length);
-        const data = indecies.map(index => {
-            let url;
-            let meme = responseMemes[index];
-            switch (meme.type) {
-                case 'Photo':
-                    url = meme.images.image700.url;
-                    break;
-                case 'Animated':
-                    url = meme.images.image460svwm.url;
-                    break;
-                case 'Video':
-                    url = `https://youtu.be/${meme.video.id}`;
-                    break;
-            }
-            return {
-                type: meme.type,
-                title: meme.title,
-                id: meme.id,
-                url
-            };
-        });
-        res.status(200).json(responses_1.successRes(data));
+exports.getRandomMemes = (req, res) => __awaiter(this, void 0, void 0, function* () {
+    let responseMemes = null;
+    let cursor = null;
+    for (let i = 0; i < 5; i++) {
+        const json = yield request(responseMemes ? memesEndpoint + '?' + cursor : memesEndpoint);
+        let parsed = JSON.parse(json);
+        let response = parsed.data.posts.filter(post => post.type !== 'Article');
+        if (!responseMemes) {
+            responseMemes = response;
+        }
+        else {
+            responseMemes = [...responseMemes, ...response];
+        }
+        cursor = parsed.data.nextCursor;
+    }
+    const indecies = random_1.getRandomNumbers(responseMemes.length - 1);
+    const data = indecies.map(index => {
+        let url;
+        let meme = responseMemes[index];
+        switch (meme.type) {
+            case 'Photo':
+                url = meme.images.image700.url;
+                break;
+            case 'Animated':
+                url = meme.images.image460svwm.url;
+                break;
+            case 'Video':
+                url = `https://youtu.be/${meme.video.id}`;
+                break;
+        }
+        return {
+            type: meme.type,
+            title: meme.title,
+            id: meme.id,
+            url
+        };
     });
-};
+    res.status(200).json(responses_1.successRes(data));
+});
 //# sourceMappingURL=randomMeme.js.map
--- server/src/controllers/randomMeme.ts
+++ server/src/controllers/randomMeme.ts
@@ -5,31 +5,41 @@ import { getRandomNumbers } from '../utils/random'
 
 const memesEndpoint = 'https://9gag.com/v1/group-posts/group/default/type/hot'
 
-export const getRandomMemes = (req: Request, res: Response) => {
-  request(memesEndpoint).then(json => {
-    const responseMemes = JSON.parse(json).data.posts.filter(post => post.type !== 'Article')
-    const indecies = getRandomNumbers(responseMemes.length)
-    const data = indecies.map(index => {
-      let url: string
-      let meme = responseMemes[index]
-      switch (meme.type) {
-        case 'Photo':
-          url = meme.images.image700.url
-          break
-        case 'Animated':
-          url = meme.images.image460svwm.url
-          break
-        case 'Video':
-          url = `https://youtu.be/${meme.video.id}`
-          break
-      }
-      return {
-        type: meme.type,
-        title: meme.title,
-        id: meme.id,
-        url
-      }
-    })
-    res.status(200).json(successRes(data))
+export const getRandomMemes = async (req: Request, res: Response) => {
+  let responseMemes = null
+  let cursor = null
+  for (let i = 0; i < 5; i++) {
+    const json = await request(responseMemes ? memesEndpoint + '?' + cursor : memesEndpoint)
+    let parsed = JSON.parse(json)
+    let response = parsed.data.posts.filter(post => post.type !== 'Article')
+    if (!responseMemes) {
+      responseMemes = response
+    } else {
+      responseMemes = [...responseMemes, ...response]
+    }
+    cursor = parsed.data.nextCursor
+  }
+  const indecies = getRandomNumbers(responseMemes.length - 1)
+  const data = indecies.map(index => {
+    let url: string
+    let meme = responseMemes[index]
+    switch (meme.type) {
+      case 'Photo':
+        url = meme.images.image700.url
+        break
+      case 'Animated':
+        url = meme.images.image460svwm.url
+        break
+      case 'Video':
+        url = `https://youtu.be/${meme.video.id}`
+        break
+    }
+    return {
+      type: meme.type,
+      title: meme.title,
+      id: meme.id,
+      url
+    }
   })
+  res.status(200).json(successRes(data))
 }
