d22c2744ce1ade7b4c40c953c45f7954129436e7
mightyYaroslav <yaroslavsmth@gmail.com>
2018-01-18 18:49:52 +0200

    Updated inc rate quey

--- server/src/controllers/memeIncRate.ts
+++ server/src/controllers/memeIncRate.ts
@@ -3,16 +3,9 @@ import MemeModel from '../models/Meme'
 import { successRes, failRes } from '../utils/responses'
 
 export function memeIncRating (req: Request, res: Response, next: NextFunction) {
-  res.json(successRes(MemeModel.findById(req.query.id, (err, meme) => {
-    if (err) {
-      res.json({ success: false, message: err })
-    }
-    meme.rating++
-    meme.save((err, updated) => {
-      if (err) {
-        res.json({ success: false, message: err })
-      }
-      res.json(successRes({ updated }))
-    })
-  })))
+  res.json(successRes(MemeModel.findByIdAndUpdate(
+    req.query.id,
+    { $inc : { 'rating' : 1 } },
+    { upsert: true, setDefaultsOnInsert: true }
+    )))
 }
