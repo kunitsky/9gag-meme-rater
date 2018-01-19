04c9299256d5bfe6710898e2de9ab3fa9b8ea846
mightyYaroslav <yaroslavsmth@gmail.com>
2018-01-19 20:23:07 +0200

    new gitignore

--- server/build/controllers/meme.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Meme {
-    getById(id) {
-    }
-}
-exports.Meme = Meme;
-//# sourceMappingURL=meme.js.map
--- server/build/utils/convertType.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertType = (type) => type === 'Video' ? 'VIDEO' : 'IMAGE';
-//# sourceMappingURL=convertType.js.map
