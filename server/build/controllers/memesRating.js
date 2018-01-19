7cc9a1d5e8ccdb760be45ed53267e7ab803fe4ea
mightyYaroslav <yaroslavsmth@gmail.com>
2018-01-19 13:09:50 +0200

    added async to rating func

--- server/build/controllers/memesRating.js
+++ server/build/controllers/memesRating.js
@@ -1,11 +1,21 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Meme_1 = require("../models/Meme");
 const responses_1 = require("../utils/responses");
 const memesPerPage = 10;
 function memesRating(req, res, next) {
-    const page = req.query.page || 0;
-    res.json(responses_1.successRes(Meme_1.default.find().sort('rating').skip(page * memesPerPage).limit(memesPerPage)));
+    return __awaiter(this, void 0, void 0, function* () {
+        const page = req.query.page || 0;
+        res.json(responses_1.successRes(yield Meme_1.default.find().sort('rating').skip(page * memesPerPage).limit(memesPerPage)));
+    });
 }
 exports.memesRating = memesRating;
 //# sourceMappingURL=memesRating.js.map
--- server/src/controllers/memesRating.ts
+++ server/src/controllers/memesRating.ts
@@ -4,7 +4,7 @@ import { successRes, failRes } from '../utils/responses'
 
 const memesPerPage = 10
 
-export function memesRating (req: Request, res: Response, next: NextFunction) {
+export async function memesRating (req: Request, res: Response, next: NextFunction) {
   const page = req.query.page || 0
-  res.json(successRes(MemeModel.find().sort('rating').skip(page * memesPerPage).limit(memesPerPage)))
+  res.json(successRes(await MemeModel.find().sort('rating').skip(page * memesPerPage).limit(memesPerPage)))
 }
