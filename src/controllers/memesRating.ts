7e4719a5729c84b2a66f3617735b9f9fa0d702df
mightyYaroslav <yaroslavsmth@gmail.com>
2018-01-21 14:20:24 +0200

    renamed page to prevCursor

--- src/controllers/memesRating.ts
+++ src/controllers/memesRating.ts
@@ -5,12 +5,12 @@ import { successRes, failRes } from '../utils/responses'
 const memesPerPage = 10
 
 export async function memesRating (req: Request, res: Response, next: NextFunction) {
-  const page = ~~req.query.cursor || 0
+  const prevCursor = ~~req.query.cursor || 0
   const pagesCount = await MemeModel.find().count() / memesPerPage
 
-  let cursor = page >= 0 && page < pagesCount - 1 ? page + 1 : null
-  if (page <= pagesCount - 1 || page >= 0) {
-    const memes = await MemeModel.find().sort([['rating', -1]]).skip(page * memesPerPage).limit(memesPerPage)
+  let cursor = prevCursor >= 0 && prevCursor < pagesCount - 1 ? prevCursor + 1 : null
+  if (prevCursor <= pagesCount - 1 || prevCursor >= 0) {
+    const memes = await MemeModel.find().sort([['rating', -1]]).skip(prevCursor * memesPerPage).limit(memesPerPage)
     res.json(successRes({ memes, cursor }))
   } else {
     res.json({ cursor })
